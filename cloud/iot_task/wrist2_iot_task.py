3ce1a68af9c5189e83971a46fb4c45c89af7b9fe
gayanvoice <gayan.kuruppu@live.com>
2023-07-04 12:21:21 +0100

    feat: wrist2

---
+++ cloud/iot_task/wrist2_iot_task.py
@@ -0,0 +1,37 @@
+import logging
+import asyncio
+import json
+
+
+class Wrist2IotTask:
+
+    def __init__(self, device):
+        self.__device = device
+        self.__json_file = None
+        self.__running = True
+
+    def terminate(self):
+        self.__running = False
+
+    async def connect(self):
+        logging.info("wrist2_iot_task.connect:starting")
+
+        while self.__running:
+            try:
+                cache_json_file = open('cache.json')
+                data_object = json.load(cache_json_file)
+                cache_json_file.close()
+                telemetry = {"position": data_object['base_model']['_position'],
+                             "temperature": data_object['base_model']['_temperature'],
+                             "voltage": data_object['base_model']['_voltage']}
+                logging.info("wrist2_iot_task.connect:" + str(telemetry))
+                await self.__device.send_telemetry(telemetry)
+                await asyncio.sleep(5)
+
+            except Exception as ex:
+                await asyncio.sleep(5)
+                logging.error("wrist2_iot_task.connect.while:error={error}".format(error=str(ex)))
+
+        logging.debug("wrist2_iot_task.connect:complete")
+
+
